cd7c5aebbabefff5c6f0d41276a8fb4cad8b5142
Charles Pierse <charlespierse@gmail.com>
2020-04-24 19:29:37 +0100

    Completed fetch module

--- etl/download/fetch.go
+++ etl/download/fetch.go
@@ -1,66 +1,87 @@
 package download
 
 import (
-	"cloud.google.com/go/bigquery"
 	"context"
-	"google.golang.org/api/iterator"
 	"log"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
 )
 
-// ArticlePost is a struct
-// for storing the results of
-// our query
-type ArticlePost struct {
-	Title     string
-	URL       string
-	Score     int
-	Timestamp string
-	ID        int
-	Type      string
+// Post is a struct for storing
+// the results of our query to the hacker news
+// table in bigquery
+type Post struct {
+	Title     string    `bigquery:"title"`
+	URL       string    `bigquery:"url"`
+	Score     int       `bigquery:"score"`
+	Timestamp time.Time `bigquery:"timestamp"`
+	ID        int       `bigquery:"id"`
+	Type      string    `bigquery:"type"`
+}
+
+// GetPosts returns a slice of
+// posts made up of Post structs returned
+// from our bigquery query
+func GetPosts() []Post {
+	log.Println("Starting query to BigQuery")
+	client, ctx := _setUpClientContext()
+	log.Println("Set Up BigQuery Client")
+	it := _query(ctx, client)
+	log.Println("Got Query")
+	res := _createPosts(it)
+	log.Println("Successfully fetched and created posts")
+	return res
 }
 
 func _setUpClientContext() (*bigquery.Client, context.Context) {
 
 	ctx := context.Background()
-	client, err := bigquery.NewClient(ctx, "hacker-news-analysis-275115")
+	// Enter your own project ID here
+	projectID := "hacker-news-analysis-275115"
+
+	client, err := bigquery.NewClient(ctx, projectID)
 	if err != nil {
 		log.Fatalf("Error setting up new client %v", err)
 	}
 	return client, ctx
 }
 
-func _query(context context.Context, client *bigquery.Client) {
-
-}
-
-// Fetch Does
-func Fetch() []ArticlePost {
-
-	client, ctx := _setUpClientContext()
-
-	q := client.Query(`Select title, url, score, timestamp, id, type
-	FROM ` + "`bigquery-public-data.hacker_news.full`" + `
-	WHERE title != ""
-	`)
+func _query(ctx context.Context, client *bigquery.Client) *bigquery.RowIterator {
+	q := client.Query(
+		`Select COALESCE(title,"None"), COALESCE(url,"None"), score, timestamp, id, type
+		FROM ` + "`bigquery-public-data.hacker_news.full`" + `
+		WHERE title != ""
+					`)
 
 	it, err := q.Read(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return it
+
+}
+
+func _createPosts(it *bigquery.RowIterator) []Post {
+	var posts []Post
+	rowsAdded := 0
+
 	for {
-		var values []bigquery.Value
-		err := it.Next(&values)
+		var p Post
+		err := it.Next(&p)
+		posts = append(posts, p)
+		rowsAdded++
+		if rowsAdded%10000 == 0 {
+			log.Printf("Rows added so far is %v", rowsAdded)
+		}
 		if err == iterator.Done {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("Encountered error with iterator: %v", err)
 		}
-		log.Print(values[1])
-
-		break
 	}
-
-	return []ArticlePost{}
+	return posts
 
 }
--- etl/main.go
+++ etl/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/cdpierse/hacker_news_NLP_score_prediction/etl/download"
+	"log"
 )
 
 func main(){
 	fmt.Println("Hello world")
-	download.Fetch()
+	posts := download.GetPosts()
+	log.Println(len(posts))
 
 
 }
