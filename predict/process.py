630e6c4a52fbe8ae0ad1d1c7cda02d12aff335c2
Charles Pierse <charlespierse@gmail.com>
2020-05-05 16:44:57 +0100

    Add Process class title transforms

--- predict/process.py
+++ predict/process.py
@@ -3,6 +3,7 @@ import logging
 import pandas as pd
 import psycopg2 as pg
 import tldextract
+from string import punctuation
 from db import Connection
 
 
@@ -20,10 +21,37 @@ class Process:
                 self.posts = self.posts.convert_dtypes()
                 logging.info("Retrieved posts from database")
             except Exception as e:
-                logging.error(e)
+                logging.error(e + "Please ensure container is up and running.")
+
+    def prepend_domain(self, r):
+        url = r.url
+        if url == "empty":
+            r.title = "empty :- " + r.title
+            return r.title
+        else:
+            domain = Process.extract_domain(url)
+            r.title = domain.domain + " :- " + r.title
+            return r.title
+
+    def apply_title_transforms(self):
+        logging.info("Applying title transformations")
+        self.posts['title'] = self.posts['title'].apply(
+            Process.remove_punctuation)
+        self.posts['title'] = self.posts['title'].apply(Process.title_to_lower)
+        self.posts['title'] = self.posts.apply(self.prepend_domain, axis=1)
+        logging.info("Applied all title transforms")
 
     @staticmethod
-    def domain_extractor(url):
+    def title_to_lower(s):
+        return s.lower()
+
+    @staticmethod
+    def remove_punctuation(s):
+        "Must come first"
+        return s.translate(str.maketrans('', '', punctuation))
+
+    @staticmethod
+    def extract_domain(url):
         try:
             return tldextract.extract(url)
         except Exception as e:
@@ -33,5 +61,5 @@ class Process:
 
 
 if __name__ == "__main__":
-    # p = Process()
-    print(Process.domain_extractor("https://www.linkedin.com/"))
+    p = Process()
+    p.apply_title_transforms()
