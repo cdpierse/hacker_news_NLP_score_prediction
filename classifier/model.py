8d3f8b28e2abb5601fc7e608c06e0ce2d894569a
Charles Pierse <charlespierse@gmail.com>
2020-05-26 20:19:41 +0100

    Add class for torch dataset

---
+++ classifier/model.py
@@ -0,0 +1,8 @@
+from torch.utils.data import dataset
+
+
+class HackerNewsPostDataset(dataset):
+    def __init__(self):
+        """__init__ will be the dataset class for handling hacker
+        news post data.
+        """
